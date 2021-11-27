0e58ce67357c6604cd96fee189aaa9c3d1ac4dea
Lrd134 <larryc3200@gmail.com>
2021-11-27 04:37:26 -0500

    commit tree deeb23a7699d0243d4ce4098822ebdf90dbb9c72

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-// ./src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux"; /* code change */
+import counterReducer from "./reducers/counterReducer.js";
+import App from "./App";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import counterReducer from './reducers/counterReducer.js';
-import App from './App';
-import './index.css';
+const store = createStore(counterReducer); /* code change */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById("root"));
